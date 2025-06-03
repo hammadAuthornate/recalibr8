e06a1f054064e18601beb857c0845d240d3b6c39
hammadAuthornate <156046487+hammadAuthornate@users.noreply.github.com>
2025-06-03 16:43:05 +0500

    updated authservice and firebase config

--- lib/services/AuthService.ts
+++ lib/services/AuthService.ts
@@ -1,18 +1,16 @@
-"use server";
-
 import { AUTH } from "@/types/auth";
 import { BackendError } from "../config/error";
+import { genSaltSync, hashSync, compareSync } from "bcrypt";
 import { db } from "../config/firebase.config";
+import { collection, addDoc, getDocs, where, query } from "firebase/firestore";
 
 export default class AuthService {
-  private static AuthCollection = db.collection("auth");
+  private static AuthCollection = collection(db, "auth");
 
   static async signUp(credentials: AUTH) {
-    const previousUser = await this.AuthCollection.where(
-      "email",
-      "==",
-      credentials.email
-    ).get();
+    const previousUser = await getDocs(
+      query(this.AuthCollection, where("email", "==", credentials.email))
+    );
 
     if (!previousUser.empty) {
       throw new BackendError("User already exists.", {
@@ -21,19 +19,33 @@ export default class AuthService {
       });
     }
 
-    await this.AuthCollection.add(credentials);
+    const salt = genSaltSync(10);
+    const hash = hashSync(credentials.password, salt);
+
+    const response = await addDoc(this.AuthCollection, {
+      ...credentials,
+      password: hash,
+      type: "end-user",
+    } as Partial<AUTH>);
+
+    if (!response.id) {
+      throw new BackendError("Unable to store user info.", {
+        cause: "Please enter valid information or contact support.",
+        statusCode: 500,
+      });
+    }
 
     return {
+      id: response.id,
       email: credentials.email,
-    };
+      name: credentials.name,
+    } as AUTH;
   }
 
   static async signIn(credentials: AUTH) {
-    const userDetails = await this.AuthCollection.where(
-      "email",
-      "==",
-      credentials.email
-    ).get();
+    const userDetails = await getDocs(
+      query(this.AuthCollection, where("email", "==", credentials.email))
+    );
 
     if (userDetails.empty) {
       throw new BackendError("User does not exist.", {
@@ -42,8 +54,18 @@ export default class AuthService {
       });
     }
 
-    const userData = userDetails.docs?.at(0)?.data() as AUTH;
+    const userRef = userDetails.docs?.at(0);
+    const userData = userRef?.data() as AUTH;
 
-    return { ...userData, password: undefined };
+    const isValidPassword = compareSync(
+      credentials.password,
+      userData?.password
+    );
+
+    if (!isValidPassword) {
+      throw new BackendError("Invalid Password!", { statusCode: 400 });
+    }
+
+    return { ...userData, password: undefined, id: userRef?.id };
   }
 }
