57d52198a8c749308d54d4554572c0170a561e17
hammadAuthornate <156046487+hammadAuthornate@users.noreply.github.com>
2025-06-03 13:08:32 +0500

    updated firebase config

--- lib/config/firebase.config.ts
+++ lib/config/firebase.config.ts
@@ -1,19 +1,11 @@
 import { initializeApp, credential } from "firebase-admin";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDIxU5Pfm9GBWkqhJAUct4WSKMpeoKPw7Q",
-  authDomain: "recalibr8-dashboard.firebaseapp.com",
-  projectId: "recalibr8-dashboard",
-  storageBucket: "recalibr8-dashboard.firebasestorage.app",
-  messagingSenderId: "21918464362",
-  appId: "1:21918464362:web:f88e9a43cac79cc3208d13",
-};
-
 const admin = initializeApp({
-  credential: credential.cert(
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    require("../../recalibr8-dashboard-firebase-adminsdk-fbsvc-6ddcc952ab.json")
-  ),
+  credential: credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  }),
 });
 
 const db = admin.firestore();
